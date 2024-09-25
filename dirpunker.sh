05488472fa3eae7a34d53454fcc96123f0007d6e
Aditya Singh <dr.singhaditya@hotmail.com>
2024-09-25 15:54:09 +0200

    Added method to set rmpunk.R location in dirpunker.sh

--- dirpunker.sh
+++ dirpunker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # This BASH script finds all .R or .Rmd files in the given directory and runs the rmpunk.R Rscript on these files
-
+# Get the complete path of the current directory and set rmpunk.R location
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+RMPUNK=$DIR/rmpunk.R
 # Check if the user has provided a directory
 if [ $# -eq 0 ]; then
   echo "Please provide a directory"
@@ -24,10 +26,10 @@ echo Files found: $files
 
 if [ -x "$(command -v parallel)" ]; then
   echo "GNU Parallel available: Using parallel processing"
-  echo $files | tr ' ' '\n' | parallel Rscript rmpunk.R {}
+  echo $files | tr ' ' '\n' | parallel Rscript "$RMPUNK" {}
 else
   echo "GNU parallel is not installed. Running the script sequentially"
   for file in $files; do
-    Rscript rmpunk.R $file
+    Rscript "$RMPUNK" $file
   done
 fi
